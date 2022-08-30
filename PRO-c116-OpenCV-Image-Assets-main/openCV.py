caf963d003f3328c00c1f927948aedea5048693f
ShauryaAttal <87157875+ShauryaAttal@users.noreply.github.com>
2022-08-30 18:47:11 -0500

    Add files via upload

---
+++ PRO-c116-OpenCV-Image-Assets-main/openCV.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("butterfly.jpg")
+cv2.imshow("Butterfly", img)
+
+print(img)
+
+gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+cv2.imshow("Gray Fly", gray_img)
+
+cv2.waitKey(0)
